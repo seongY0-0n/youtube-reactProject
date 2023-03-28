b2ef1684c127af5ffc067261bcae2f03bb966b9e
seriokim <atomyoun@gmail.com>
2023-03-28 18:38:37 +0900

    [2023.03.28] videoDetail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { format, register } from "timeago.js";
+import koLocale from "timeago.js/lib/lang/ko";
+import { useNavigate } from "react-router-dom";
 
-export default function VideoCard({ video }) {
-  return <div>{video.snippet.title}</div>;
+register("ko", koLocale);
+export default function VideoCard({ video, type }) {
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
+  const isList = type === "list";
+  return (
+    <li
+      className={isList ? "flex gap-1 m-2" : ""}
+      onClick={() => {
+        navigate(`/videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
+      <img
+        className={isList ? "w-60 mr-2" : "w-full"}
+        src={thumbnails.medium.url}
+        alt={title}
+      />
+      <div>
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{format(publishedAt, "ko")}</p>
+      </div>
+    </li>
+  );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import VideoCard from "./../components/VideoCard";
-import { search } from "../api/youtube";
-import FakeYoutube from "./../api/fakeYoutube";
-import Youtube from "./../api/youtube";
 import { useYoutubeApi } from "./../context/YoutubeApiContext";
 export default function Videos() {
   const { keyword } = useParams();
@@ -14,14 +11,12 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(["videos", keyword], () => youtube.search(keyword));
-
   return (
     <>
-      <div>Videos {keyword ? `찾음${keyword}` : "hotTrend"}</div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong...</p>}
       {videos && (
-        <ul>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
