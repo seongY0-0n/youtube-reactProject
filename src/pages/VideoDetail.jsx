352636422fceffd5ac7f92aface1d8d4be5545b9
seriokim <atomyoun@gmail.com>
2023-04-03 18:37:12 +0900

    https 수정

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -19,7 +19,7 @@ export default function VideoDetail() {
           type="text/html"
           width="100%"
           height="640"
-          src={`http://www.youtube.com/embed/${videoId}`}
+          src={`https://www.youtube.com/embed/${videoId}`}
           frameBorder="0"
           title={title}
         />
